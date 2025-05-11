629634cd6793dd81684fa482540f0180968297b1
FelixItsMe <filok5267@gmail.com>
2025-05-11 23:18:06 +0700

    Feat: add route for post and comment

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +16,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('post', PostController::class)->except('show')->scoped([
+        'post' => 'slug'
+    ]);
+    Route::resource('post.comment', CommentController::class)->only(['store', 'destroy'])
+        ->scoped(['post' => 'slug']);
 });
 
-require __DIR__.'/auth.php';
+Route::resource('post', PostController::class)->only('show')->scoped([
+    'post' => 'slug'
+]);
+
+require __DIR__ . '/auth.php';
