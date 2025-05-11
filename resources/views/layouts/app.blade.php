56ae739d17c985edd7a8d8e0f121daa19e00337d
FelixItsMe <filok5267@gmail.com>
2025-05-11 23:15:45 +0700

    Refactor: add stack for styles and scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @stack('styles')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen dark:bg-gray-900">
@@ -34,5 +36,6 @@
                 {{ $slot }}
             </main>
         </div>
+        @stack('scripts')
     </body>
 </html>
